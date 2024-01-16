2c9383333b514e7dcad0e4aa520fb06eca8fcbd6
vishal soni <vishsoni043@gmail.com>
2024-01-16 13:52:07 +0530

    refactor - cors error

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(3000);
+// }
+
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,9 +19,17 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3333);
 }
 
 bootstrap();
