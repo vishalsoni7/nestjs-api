77ec44c3a0efa876bdb4dea09b09e9798da8d797
vishal soni <vishsoni043@gmail.com>
2024-01-16 14:45:19 +0530

    fixing cors error

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,24 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import { NestExpressApplication } from '@nestjs/platform-express';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-//   app.enableCors();
-//   app.useGlobalPipes(new ValidationPipe());
-//   await app.listen(3000);
-// }
-
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+const cors = require('cors');
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.use(cors());
 
-  app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  //   optionsSuccessStatus: 204,
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
+
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
 
